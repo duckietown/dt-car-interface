7df5da9248ebb52a82505fa191ac48f9d4d3f146
Gianmarco Bernasconi <gibernas@ethz.ch>
2019-10-09 17:57:31 +0200

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -4,5 +4,5 @@ set -e
 
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
-roslaunch car_interface all.launch veh:=$VEHICLE_NAME
+roslaunch --wait car_interface all.launch veh:=$VEHICLE_NAME
 
